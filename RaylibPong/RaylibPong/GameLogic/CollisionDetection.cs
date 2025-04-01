d4c963518948e5a45a4b2de9f34b45b48e7a1d42
Mariusz <klimcio@gmail.com>
2025-04-01 21:44:25 +0200

    Score

--- RaylibPong/RaylibPong/GameLogic/CollisionDetection.cs
+++ RaylibPong/RaylibPong/GameLogic/CollisionDetection.cs
@@ -3,10 +3,30 @@ using RaylibPong.GameObjects;
 
 namespace RaylibPong.GameLogic;
 
+public enum ScreenBorder
+{
+    Top,
+    Bottom,
+    Left,
+    Right
+}
+
 public static class CollisionDetection
 {
-    public static bool IsCollidingWithScreenBorderX(this IAmA2dBeing being)
-        => being.Position.X >= Raylib.GetScreenWidth() - being.Radius || being.Position.X <= being.Radius;
+    public static (bool, ScreenBorder) IsCollidingWithScreenBorderX(this IAmA2dBeing being)
+    {
+        if (being.Position.X >= Raylib.GetScreenWidth() - being.Radius)
+            return (true, ScreenBorder.Right);
+
+        if (being.Position.X <= being.Radius)
+            return (true, ScreenBorder.Left);
+
+        return (false, ScreenBorder.Left);
+
+    }
+
+    //public static bool IsCollidingWithScreenBorderX(this IAmA2dBeing being)
+    //    => being.Position.X >= Raylib.GetScreenWidth() - being.Radius || being.Position.X <= being.Radius;
 
     public static bool IsCollidingWithScreenBorderY(this IAmA2dBeing being)
         => being.Position.Y >= Raylib.GetScreenHeight() - being.Radius || being.Position.Y <= being.Radius;
--- RaylibPong/RaylibPong/GameplayLoop/GameplayPhase.cs
+++ RaylibPong/RaylibPong/GameplayLoop/GameplayPhase.cs
@@ -10,6 +10,8 @@ internal class GameplayPhase : IGamePhase
     Ball theBall;
     Paddle player1Paddle;
     Paddle player2Paddle;
+    int player1Score = 0;
+    int player2Score = 0;
 
     Settings settings;
     bool IsPause = false;
@@ -18,7 +20,7 @@ internal class GameplayPhase : IGamePhase
     {
         this.settings = settings;
 
-        theBall = InitBall();
+        theBall = InitBall(ScreenBorder.Left);
         player1Paddle = new Paddle(
             new Vector2(10, settings.VerticalCenter)
         );
@@ -27,7 +29,7 @@ internal class GameplayPhase : IGamePhase
         );
     }
 
-    private Ball InitBall() => new(
+    private Ball InitBall(ScreenBorder direction) => new(
             settings.ScreenCenter,
             initialBallSpeed: new Vector2(-5.0f, -4.0f)
         );
@@ -35,6 +37,7 @@ internal class GameplayPhase : IGamePhase
     public void Draw()
     {
         Raylib.ClearBackground(Color.RayWhite);
+        DrawCurrentScore();
 
         theBall.Draw();
         player1Paddle.Draw();
@@ -44,6 +47,12 @@ internal class GameplayPhase : IGamePhase
             Raylib.DrawText("paused", 350, 200, 30, Color.Gray);
 
         Raylib.DrawFPS(10, 10);
+    }
+
+    private void DrawCurrentScore()
+    {
+        Raylib.DrawText($"{player1Score}:{player2Score}", 
+            (int)settings.ScreenCenter.X - 50, 20, 40, Color.DarkGray);
     }
 
     public void Unload()
@@ -56,11 +65,22 @@ internal class GameplayPhase : IGamePhase
 
         if (!IsPause)
         {
-            if (theBall.IsCollidingWithScreenBorderX())
+            (bool isColliding, ScreenBorder border) = theBall.IsCollidingWithScreenBorderX();
+            
+            if (isColliding)
             {
-                // todo: point scored
-                theBall = InitBall();
+                if (border == ScreenBorder.Left)
+                {
+                    player2Score++;
+                    theBall = InitBall(ScreenBorder.Right);
+                }
+                else if (border == ScreenBorder.Right)
+                {
+                    player1Score++;
+                    theBall = InitBall(ScreenBorder.Left);
+                }
             }
+
             if (theBall.IsCollidingWithScreenBorderY())
                 theBall.InvertVerticalDirection();
 
